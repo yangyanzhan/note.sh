b2e83076b9ce7122ba52011c2201a2c99833a7f0
yangyanzhan <yangyanzhan@gmail.com>
2020-08-19 19:39:05 +0800

    1.add list action.

--- note.sh
+++ note.sh
@@ -4,6 +4,8 @@
 # I"m interested in all kinds of algorithmic problems. If you want to learn more about me, you can visit my [Youtube Channel](https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber), [Twitter Account](https://twitter.com/YangYanzhan) or [GitHub Repo](https://github.com/yangyanzhan/code-camp)
 # Also, Note.sh project is hosted on my [GitHub Repo](https://github.com/yangyanzhan/note.sh). If you like this project, don"t forget visit my GitHub Repo, star it and then follow me.
 
+action=$1
+
 setup() {
     # setup global variables
     # project root directory
@@ -34,8 +36,15 @@ setup() {
     fi
 }
 
+list() {
+    ls $note_dir
+}
+
 main() {
     setup
+    if [[ $action == "list" ]]; then
+        list
+    fi
 }
 
 main
