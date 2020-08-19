a1876dbb9272bdd975bf5c3b1880692b1b853640
yangyanzhan <yangyanzhan@gmail.com>
2020-08-19 14:54:13 +0800

    1.add colors. 2.add .note check.

--- note.sh
+++ note.sh
@@ -1,7 +1,41 @@
 #!/usr/bin/env bash
 
 # Note.sh is a text-based note keeping software completely written in shell script created by Yang Yanzhan.
-# I'm interested in all kinds of algorithmic problems. If you want to learn more about me, you can visit my [Youtube Channel](https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber), [Twitter Account](https://twitter.com/YangYanzhan) or [GitHub Repo](https://github.com/yangyanzhan/code-camp)
-# Also, Note.sh project is hosted on my [GitHub Repo](https://github.com/yangyanzhan/note.sh). If you like this project, don't forget visit my GitHub Repo, star it and then follow me.
+# I"m interested in all kinds of algorithmic problems. If you want to learn more about me, you can visit my [Youtube Channel](https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber), [Twitter Account](https://twitter.com/YangYanzhan) or [GitHub Repo](https://github.com/yangyanzhan/code-camp)
+# Also, Note.sh project is hosted on my [GitHub Repo](https://github.com/yangyanzhan/note.sh). If you like this project, don"t forget visit my GitHub Repo, star it and then follow me.
 
+setup() {
+    # setup global variables
+    # project root directory
+    root_dir=$(dirname $BASH_SOURCE)
+    # note storage directory
+    note_dir="${root_dir}/.note"
+    nocolor="\033[0m"
+    red="\033[0;31m"
+    green="\033[0;32m"
+    orange="\033[0;33m"
+    blue="\033[0;34m"
+    purple="\033[0;35m"
+    cyan="\033[0;36m"
+    lightgray="\033[0;37m"
+    darkgray="\033[1;30m"
+    lightred="\033[1;31m"
+    lightgreen="\033[1;32m"
+    yellow="\033[1;33m"
+    lightblue="\033[1;34m"
+    lightpurple="\033[1;35m"
+    lightcyan="\033[1;36m"
+    white="\033[1;37m"
+    # check project environment and setup
+    if [ ! -e $note_dir ]; then
+        echo "${yellow}creating note storage directory...${nocolor}"
+        mkdir -p $note_dir
+        echo "${green}done${nocolor}"
+    fi
+}
 
+main() {
+    setup
+}
+
+main
