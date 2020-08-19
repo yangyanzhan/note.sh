db7f7cb2619b005066895badde147e77d8dfeaa7
yangyanzhan <yangyanzhan@gmail.com>
2020-08-19 14:31:32 +0800

    1.add note.sh file. 2.add copyright and promotion comment.

---
+++ note.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Note.sh is a text-based note keeping software completely written in shell script created by Yang Yanzhan.
+# I'm interested in all kinds of algorithmic problems. If you want to learn more about me, you can visit my [Youtube Channel](https://www.youtube.com/channel/UCDkz-__gl3frqLexukpG0DA?view_as=subscriber), [Twitter Account](https://twitter.com/YangYanzhan) or [GitHub Repo](https://github.com/yangyanzhan/code-camp)
+# Also, Note.sh project is hosted on my [GitHub Repo](https://github.com/yangyanzhan/note.sh). If you like this project, don't forget visit my GitHub Repo, star it and then follow me.
+
+
